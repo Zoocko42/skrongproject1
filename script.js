e95c20edb17b8985b767e81e0938e731dbe44f24
tmorgan1987 <tmorgan1987@outlook.com>
2022-09-28 21:29:38 -0400

    cleaned up html, added some comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var searchButton = document.getElementById("searchBtn");
+searchButton.addEventListener("click",  initMap);
+
 function geoCode(input) {
   var encodedAddress = encodeURIComponent(input);
   var encodedAddressFetch = fetch(
@@ -19,26 +22,6 @@ async function initMap() {
   new window.google.maps.Map(document.getElementById("map"), options);
 }
 
-var searchButton = document.getElementById("button2");
-searchButton.addEventListener("click", initMap);
-
-//     var marker = new google.maps.Marker({
-//       position: '',
-//       map:map,
-//       icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-//     });
-
-//     var infoWindow = new google.maps.InfoWindow({
-
-//       content: '<h3>Your new home</h3>'
-//     });
-
-//     marker.addEventListener('click', function(){
-//       infoWindow.open(map, marker)
-//     })
-
-//   }
-
 var autocomplete;
 function initAutocomplete() {
   autocomplete = new google.maps.places.Autocomplete(
@@ -51,10 +34,10 @@ function initAutocomplete() {
   );
   autocomplete.addEventListener("place_changed", onPlaceChanged);
 }
+
 function onPlaceChanged() {
   var place = autocomplete.getPlace();
-
-  if (place.geometry) {
+	if (place.geometry) {
     document.getElementById("autocomplete").placeholder = "Search";
   } else {
     document.getElementById("details").innerHTML = place.name;
